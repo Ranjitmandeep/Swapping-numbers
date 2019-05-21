6781834ae5c423c1b02b90daad0af136b9a834e2
Mandeep Ranjit <Mandeep Ranjit@LAPTOP-SMV5472A>
2019-05-22 11:31:42 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swapping_numbers
+{
+    class Program
+    {
+        // This program will swap the numbers 5 and 6
+        static void Main(string[] args)
+        {
+
+
+            int number5, number6, temp;
+            Console.Write("\n Put number 5 : ");
+            number5 = int.Parse(Console.ReadLine());
+            Console.Write("\n Put  number 6 : ");
+            number6 = int.Parse(Console.ReadLine());
+            temp = number5;
+            number5 = number6;
+            number6 = temp;
+            Console.Write("\nAfter Swapping : ");
+            Console.Write("\nFirst Number : " + number5);
+            Console.Write("\nSecond Number : " + number6);
+            Console.Read();
+        }
+
+    }
+}
